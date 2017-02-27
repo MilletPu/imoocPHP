a956938aa1440fd431c7654ed4fe099027f36663
milletpu <milletpu@gmail.com>
2017-02-27 20:55:57 +0800

    学习： 1，获取登录页面中的username和password； 2，封装后的dialog的使用； 3，ajax异步请求（login.js）。

--- Public/js/admin/login.js
+++ Public/js/admin/login.js
@@ -6,22 +6,23 @@
 
 var login = {
     check : function(){
-        //获取登录页面中的用户名 和 密码
+
+        //获取登录页面中的'用户名'和'密码'  ->   $('input[name = "username"]').val();
         var username = $('input[name = "username"]').val();
         var password = $('input[name = "password"]').val();
         if (!username){
             dialog.error("用户名不能为空");
         }
-        if (!password){
+        if (!password) {
             dialog.error("密码不能为空");
         }
 
+        //执行ajax异步请求  ->   $.post;
         var url = "/index.php?m=admin&c=login&a=check";
         var data = {'username':username, "password":password};
-        //执行异步请求 ajax   $.post;
         $.post(url,data,function(result){
 
         });
     }
 
-}
+};
